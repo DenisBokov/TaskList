05e8d905ae28062540e7fc9d291d883eb75f7d0f
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-08-22 21:39:01 +0300

    Add  "Save" and "Cancel" button

--- TaskList/TaskViewController.swift
+++ TaskList/TaskViewController.swift
@@ -8,10 +8,81 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    
+    private lazy var taskTextFild: UITextField = {
+        let textFild = UITextField()
+        textFild.borderStyle = .roundedRect
+        textFild.placeholder = "New Task"
+        return textFild
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.attributedTitle = AttributedString("Save Task", attributes: attributes)
+        buttonConfiguration.baseBackgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        buttonConfiguration.title = "Save Task"
+        return UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
+            dismiss(animated: true)
+        })
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.attributedTitle = AttributedString("Cancel", attributes: attributes)
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        buttonConfiguration.title = "Save Task"
+        return UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
+            dismiss(animated: true)
+        })
+    }()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
+        view.backgroundColor = .lightGray
+        setupSubviews(taskTextFild, saveButton, cancelButton)
+        setConstraints()
+    }
+    
+    private func setupSubviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
+    private func setConstraints() {
+        taskTextFild.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            taskTextFild.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            taskTextFild.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            taskTextFild.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextFild.bottomAnchor, constant: 50),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 50),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80)
+        ])
     }
 }
