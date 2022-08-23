54df458340bd7a64610bfc5fbb358ed567b70ae6
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-08-23 08:37:43 +0300

    Work on functionality

--- TaskList/TaskListViewController.swift
+++ TaskList/TaskListViewController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
+protocol TaskViewControllerDelegate {
+    func reloadData()
+}
+
 class TaskListViewController: UITableViewController {
+    
+    private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let cellID = "task"
+    private var taskList: [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         view.backgroundColor = .cyan
         setupNavigationBar()
+        fetchData()
     }
 
     private func setupNavigationBar() {
@@ -40,7 +51,41 @@ class TaskListViewController: UITableViewController {
     
     @objc private func addNewTask() {
         let newTaskViewController = TaskViewController()
+//        newTaskViewController.modalPresentationStyle = .fullScreen
+        newTaskViewController.delegate = self
         present(newTaskViewController, animated: true)
+    }
+    
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            taskList = try  viewContext.fetch(fetchRequest)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
     }
 }
 
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension TaskListViewController: TaskViewControllerDelegate {
+    func reloadData() {
+        fetchData()
+        tableView.reloadData()
+    }
+}
--- TaskList/TaskViewController.swift
+++ TaskList/TaskViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskViewController: UIViewController {
+    
+    var delegate: TaskViewControllerDelegate!
+    
+    private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     private lazy var taskTextFild: UITextField = {
         let textFild = UITextField()
@@ -17,34 +22,23 @@ class TaskViewController: UIViewController {
     }()
     
     private lazy var saveButton: UIButton = {
-        var attributes = AttributeContainer()
-        attributes.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.attributedTitle = AttributedString("Save Task", attributes: attributes)
-        buttonConfiguration.baseBackgroundColor = UIColor(
-            red: 21/255,
-            green: 101/255,
-            blue: 192/255,
-            alpha: 194/255
+        setupButton(
+            with: "Save Task",
+            and: UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 194/255),
+            action: UIAction { [unowned self] _ in
+                save()
+            }
         )
-        buttonConfiguration.title = "Save Task"
-        return UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
-            dismiss(animated: true)
-        })
     }()
     
     private lazy var cancelButton: UIButton = {
-        var attributes = AttributeContainer()
-        attributes.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.attributedTitle = AttributedString("Cancel", attributes: attributes)
-        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-        buttonConfiguration.title = "Save Task"
-        return UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
-            dismiss(animated: true)
-        })
+        setupButton(
+            with: "Cancel",
+            and: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),
+            action: UIAction { [unowned self] _ in
+                dismiss(animated: true)
+            }
+        )
     }()
  
     override func viewDidLoad() {
@@ -58,6 +52,16 @@ class TaskViewController: UIViewController {
         subviews.forEach { subview in
             view.addSubview(subview)
         }
+    }
+    
+    private func setupButton(with titel: String, and colorButton: UIColor, action: UIAction) -> UIButton {
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.attributedTitle = AttributedString(titel, attributes: attributes)
+        buttonConfiguration.baseBackgroundColor = colorButton
+        return UIButton(configuration: buttonConfiguration, primaryAction: action)
     }
     
     private func setConstraints() {
@@ -84,5 +88,21 @@ class TaskViewController: UIViewController {
             cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
             cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80)
         ])
+    }
+    
+    private func save() {
+        let task = Task(context: viewContext)
+        task.title = taskTextFild.text
+        
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        delegate.reloadData()
+        dismiss(animated: true)
     }
 }
