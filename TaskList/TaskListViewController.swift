fe3662d3b432c20bec5f89978b9ac42588e7924f
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-01 16:26:21 +0300

    Добавление Task View Controller`a

---
+++ TaskList/TaskListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TaskListViewController.swift
+//  TaskList
+//
+//  Created by Denis Bokov on 01.08.2024.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 106, green: 81, blue: 59, alpha: 1)
+    }
+
+
+}
+
+// 177,149,118 106,81,59
---
+++ TaskList/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  TaskList
+//
+//  Created by Denis Bokov on 01.08.2024.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(
+            red: 253/255,
+            green: 228/255,
+            blue: 196/255,
+            alpha: 1
+        )
+    }
+}
