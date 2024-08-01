1f971a0ceb6f1740beb80303f793c9e4c20efed3
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-01 19:03:49 +0300

    Настройка view TaskViewController

--- TaskList/TaskViewController.swift
+++ TaskList/TaskViewController.swift
@@ -8,6 +8,27 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    private lazy var taskTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "New Task"
+        return textField
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = "Save Task"
+        buttonConfiguration.baseBackgroundColor = UIColor(
+            red: 166/255,
+            green: 123/255,
+            blue: 91/255,
+            alpha: 1
+        )
+        return UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [ unowned self ] _ in
+            dismiss(animated: true)
+        })
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +38,31 @@ class TaskViewController: UIViewController {
             blue: 196/255,
             alpha: 1
         )
+        
+        setrupSabviews(taskTextField, saveButton)
+        setupConstraints()
     }
+    
+    private func setrupSabviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
+    private func setupConstraints() {
+        taskTextField.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            taskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            
+            saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 40),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+
+    }
+
 }
