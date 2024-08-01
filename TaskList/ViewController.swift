4382b3c218bbfe405e50db1305e7ebd771dfc6f3
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-01 13:04:40 +0300

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Denis Bokov on 01.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
