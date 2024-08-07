92724ffe14533af9a9f2452893799cc275f81a12
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-07 17:34:43 +0300

    Прикрутил SearchController. Зачем? Не знаю:)

--- TaskList/TaskListViewController.swift
+++ TaskList/TaskListViewController.swift
@@ -13,8 +13,17 @@ protocol TaskViewControllerDelegate {
 
 class TaskListViewController: UITableViewController {
     
+    private let searchController = UISearchController(searchResultsController: nil)
     private let callID = "task"
     private var taskList: [Task] = []
+    private var filtredTasks: [Task] = []
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        searchController.isActive && !searchBarIsEmpty
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +34,7 @@ class TaskListViewController: UITableViewController {
             alpha: 1
         )
         
+        setupSearchController()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: callID)
         
         setupNavigationBar()
@@ -63,16 +73,37 @@ class TaskListViewController: UITableViewController {
         taskVC.delegate = self
         present(taskVC, animated: true)
     }
+    
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
 }
 
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        taskList.count
+//        if isFiltering {
+//            return filtredTasks.count
+//        }
+//        
+//        return taskList.count
+        
+        return isFiltering ? filtredTasks.count : taskList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: callID, for: indexPath)
-        let task = taskList[indexPath.row]
+        var task: Task
+        
+        if isFiltering {
+            task = filtredTasks[indexPath.row]
+        } else {
+            task = taskList[indexPath.row]
+        }
+//        let task = taskList[indexPath.row]
         var content = cell.defaultContentConfiguration()
         content.text = task.title
         cell.contentConfiguration = content
@@ -83,6 +114,19 @@ extension TaskListViewController {
 extension TaskListViewController: TaskViewControllerDelegate {
     func reloadData() {
         taskList = StorageManager.shared.fetch()
+        tableView.reloadData()
+    }
+}
+
+extension TaskListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text ?? "")
+    }
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        filtredTasks = taskList.filter({ task in
+            task.title!.lowercased().contains(searchText.lowercased())
+        })
         tableView.reloadData()
     }
 }
